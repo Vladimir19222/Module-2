3fa1510f3c8286e43ebee4cca7ad91c3b5883386
Vladimir19222 <vi.karasev@mail.ru>
2024-12-25 11:38:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#  "1st program"
+print((9 ** 0.5) * 5)
+
+#  "2nd program"
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+#  "3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == 2 * (2 + 2))
+
+#  "4th program"
+s = '123.456'
+real_number = float(s)
+real_number = real_number * 10
+integer_number = int(real_number)
+number = integer_number % 10
+print(number)
